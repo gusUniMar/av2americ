d22b13274418ab763018457c649e704c0c15ddaa
Carlos Dirceu Rios Rodrigues Neto <carlosdirceu519@gmail.com>
2024-10-22 22:00:25 -0300

    segundo

--- app.js
+++ app.js
@@ -5,39 +5,173 @@ const PORT = 3000;
 app.get('/', (req, res) => {
     res.send(`
         <html>
-            <head><title>Página Inicial</title></head>
-            <body>
-                <h1>Bem-vindo à Página Inicial!</h1>
-                <p><a href="/sobre">Sobre</a></p>
-                <p><a href="/contato">Contato</a></p>
-            </body>
-        </html>
+    <head><title>Américo Tadeu</title>
+    <style>
+    *{
+    margin: 0;
+    padding: 0;}
+
+    body{
+    height: 100vh;
+    width: 100vw;
+    display: flex;
+    flex-flow: column;
+    text-align: center;
+    justify-content: center;
+    align-items: center;
+    background-image: url(https://media.tenor.com/bpNg1Db8CsEAAAAM/dance.gif);
+    background-size: cover;
+    font-family: Arial, Helvetica, sans-serif;}
+
+    .container{
+        display: flex;
+        gap: 1rem;
+        flex-flow: column;
+        justify-content: center;
+        width: 40%;
+        height: 30%;
+        background-color: rgba(179, 179, 179, 0.884);
+        border-radius: 8px;
+    }
+
+    .container a{
+        text-decoration: none;
+        color: #f1f1f1;
+    }
+
+    .container a:hover{
+        text-decoration: underline;
+    }
+    </style>
+    </head>
+    <body>
+        <div class="container">
+            <h1>Olá americo, estamos juntos</h1>
+            <h2><a href="/sobre">Rock</a></h2>
+            <h2><a href="/contato">Supostamente rock</a></h2>
+        </div>
+    </body>
+</html>
     `);
 });
 
 app.get('/sobre', (req, res) => {
     res.send(`
-        <html>
-            <head><title>Sobre</title></head>
-            <body>
-                <h1>Sobre Nós</h1>
-                <p>Esta é uma aplicação de exemplo para demonstrar rotas em Node.js com Express.</p>
-                <p><a href="/">Voltar à Página Inicial</a></p>
-            </body>
-        </html>
+       <html>
+    <head><title>Américo Tadeu</title>
+    <style>
+    *{
+    margin: 0;
+    padding: 0;}
+
+    body{
+    height: 100vh;
+    width: 100vw;
+    display: flex;
+    flex-flow: column;
+    text-align: center;
+    justify-content: center;
+    align-items: center;
+    background-image: url(https://media.tenor.com/bpNg1Db8CsEAAAAM/dance.gif);
+    background-size: cover;
+    font-family: Arial, Helvetica, sans-serif;}
+
+    .container{
+        display: flex;
+        justify-content: center;
+        width: 40%;
+        height: 30%;
+        background-color: rgba(179, 179, 179, 0.884);
+        border-radius: 8px;
+        align-items: center;
+    }
+
+    .container img{
+       max-width: 160px;
+    }
+    .container a{
+        text-decoration: none;
+        color: #f1f1f1;
+    }
+
+    .container a:hover{
+        text-decoration: underline;
+    }
+
+    </style>
+    </head>
+    <body>
+        <div class="container">
+            <img src="https://i.gifer.com/origin/f5/f5026f5ddbdfacfeab04e4d078175c15_w200.gif" alt="">
+            <p><a href="./contato">Supostamente rock</a></p>
+        </div>
+    </body>
+</html>
     `);
 });
 
 app.get('/contato', (req, res) => {
     res.send(`
         <html>
-            <head><title>Contato</title></head>
-            <body>
-                <h1>Entre em Contato Conosco</h1>
-                <p>Envie um e-mail para contato@exemplo.com</p>
-                <p><a href="/">Voltar à Página Inicial</a></p>
-            </body>
-        </html>
+    <head><title>Américo Tadeu</title>
+    <style>
+    *{
+    margin: 0;
+    padding: 0;}
+
+    body{
+    height: 100vh;
+    width: 100vw;
+    display: flex;
+    flex-flow: column;
+    text-align: center;
+    justify-content: center;
+    align-items: center;
+    background-image: url(https://diaderockbaby.wordpress.com/wp-content/uploads/2014/08/waynes-world.gif);
+    background-size: cover;
+    font-family: Arial, Helvetica, sans-serif;}
+
+    .container{
+        display: flex;
+        justify-content: center;
+        width: 40%;
+        height: 30%;
+        background-color: rgba(179, 179, 179, 0.884);
+        border-radius: 8px;
+        align-items: center;
+        flex-flow: column;
+        gap: 0.5rem;
+    }
+
+    .container img{
+       max-width: 160px;
+    }
+    .container a{
+        text-decoration: none;
+        color: #f1f1f1;
+    }
+
+    .container a:hover{
+        text-decoration: underline;
+    }
+    h2{
+        color: red;
+    }
+
+    p{
+        text-decoration:line-through;
+    }
+
+    </style>
+    </head>
+    <body>
+        <div class="container">
+            <h1>Américo me contrata</h1>
+            <h2>Nós vamos trazer o rock</h2>
+            <p>auuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuu</p>
+        </div>
+    </body>
+</html>
     `);
 });
 
